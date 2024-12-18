4c23c39dc4dffebdc6827c531b08a246165feb04
immortalWebDev <pgbadgujar007@gmail.com>
2024-12-18 17:21:30 +0530

    Added basic header

---
+++ components/Header.jsx
@@ -0,0 +1,15 @@
+
+export default function Header() {
+
+  
+  return (
+    <header>
+      <div className="header-content">
+        <h2 className="title">
+          <p>The Nations Database</p>
+        </h2>
+        Mode options
+      </div>
+    </header>
+  )
+}
