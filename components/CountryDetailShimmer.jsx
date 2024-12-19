10380d230f32e45f8ea40157d7f6dd5b0e47ceff
immortalWebDev <pgbadgujar007@gmail.com>
2024-12-19 18:05:22 +0530

    Added component for detail page shimmer effect

---
+++ components/CountryDetailShimmer.jsx
@@ -0,0 +1,10 @@
+export default function CountryDetailShimmer() {
+  return (
+    <div className="country-details">
+      <div className="flag"></div>
+      <div className="">
+        <h1 className="title"></h1>
+      </div>
+    </div>
+  )
+}
