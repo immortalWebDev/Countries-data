cd9feea038ffc3b9183da2ce18897eafa1d1690d
immortalWebDev <pgbadgujar007@gmail.com>
2024-12-20 12:20:53 +0530

    Updated styles and imprted css file

--- components/CountryDetailShimmer.jsx
+++ components/CountryDetailShimmer.jsx
@@ -1,8 +1,11 @@
+import React from 'react'
+import '../styles/CountryDetailShimmer.css'
+
 export default function CountryDetailShimmer() {
   return (
-    <div className="country-details">
+    <div className="country-details shimmer">
       <div className="flag"></div>
-      <div className="">
+      <div className="details-text-container">
         <h1 className="title"></h1>
       </div>
     </div>
