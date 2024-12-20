657df628ff7e7cc362ffbe95b2f2f5188fe88d5b
immortalWebDev <pgbadgujar007@gmail.com>
2024-12-20 12:17:52 +0530

    Integrated classNames for styles and extra p for shimmer

--- components/CountriesListShimmer.jsx
+++ components/CountriesListShimmer.jsx
@@ -6,10 +6,13 @@ export default function CountriesListShimmer() {
     <div className="countries-container">
       {Array.from({ length: 10 }).map((el, i) => {
         return (
-          <div key={i} className="country">
-            <div className=""></div>
-            <div className="">
-              <h3 className="card"></h3>
+          <div key={i} className="country-card shimmer-card">
+            <div className="flag-container"></div>
+            <div className="card-text">
+              <h3 className="card-title"></h3>
+              <p></p>
+              <p></p>
+              <p></p>
             </div>
           </div>
         )
