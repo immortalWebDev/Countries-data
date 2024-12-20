c84eb5e0f59746d861a5e15f5109c2296f118682
immortalWebDev <pgbadgujar007@gmail.com>
2024-12-20 12:22:40 +0530

    Implemented scroll logic for dynamic footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,4 +1,19 @@
 export default function Footer() {
+  const [isAtBottom, setIsAtBottom] = useState(false);
+  const [isDark] = useTheme();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isBottom =
+        window.innerHeight + window.scrollY >=
+        document.documentElement.scrollHeight;
+      setIsAtBottom(isBottom);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <footer className='footer'>
