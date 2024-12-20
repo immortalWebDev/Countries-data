5c189c1da635a92729b167f7312556866f1a7f4e
immortalWebDev <pgbadgujar007@gmail.com>
2024-12-20 12:40:21 +0530

    Updated return statement and fixed bug of displaying list

--- components/CountriesList.jsx
+++ components/CountriesList.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import CountryCard from './CountryCard'
 import CountriesListShimmer from './CountriesListShimmer'
 
 export default function CountriesList({ query }) {
@@ -27,7 +28,23 @@ export default function CountriesList({ query }) {
   return (
     <>
       <div className="countries-container">
-        
+        {countriesData
+          .filter((country) =>
+            country.name.common.toLowerCase().includes(query) || country.region.toLowerCase().includes(query)
+          )
+          .map((country) => {
+            return (
+              <CountryCard
+                key={country.name.common}
+                name={country.name.common}
+                flag={country.flags.svg}
+                population={country.population}
+                region={country.region}
+                capital={country.capital?.[0]}
+                data={country}
+              />
+            )
+          })}
       </div>
     </>
   )
