3fd5b4480fedb53be28c4e39b032d8ae5a0f76e2
immortalWebDev <pgbadgujar007@gmail.com>
2024-12-21 13:35:33 +0530

    Updated Error fallback component

--- components/Error.jsx
+++ components/Error.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-import { useRouteError } from 'react-router-dom'
+import React from 'react';
+import { useRouteError } from 'react-router-dom';
+import '../styles/Error.css'; 
 
 export default function Error() {
-  const error = useRouteError()
+  const error = useRouteError();
   console.log(error);
-  return <div>Something went wrong. {error.status}</div>
+
+  return (
+    <div className="error-container">
+      <h1>Oops!</h1>
+      <p>It seems the page you're looking for doesn't exist or something went wrong.</p>
+      {error && <p className="error-code">Error Code: {error.status || 'Unknown'}</p>}
+      <a href="/" className="home-link">Go back to Home</a>
+    </div>
+  );
 }
